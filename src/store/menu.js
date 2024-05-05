fff97c49daadb7ef6691829b4e8a8c3abf5accf0
Cristhian Daza <cristian.d.b@hotmail.com>
2024-05-05 15:25:18 -0500

    feat(menu.js): add in localStorage the menu with life of 24 hours

--- src/store/menu.js
+++ src/store/menu.js
@@ -9,6 +9,21 @@ export const useMenuStore = defineStore('menu', {
     }),
     actions: {
         async getMenu() {
+            const menuLocalStorage = localStorage.getItem('menu');
+            const lastUpdateLocalStorage = localStorage.getItem('menuLastUpdate');
+            if (menuLocalStorage && lastUpdateLocalStorage) {
+                const lastUpdateDate = new Date(lastUpdateLocalStorage);
+                const now = new Date();
+
+                const diffInMs = now - lastUpdateDate;
+                const diffInHours = diffInMs / (1000 * 60 * 60);
+
+                if (diffInHours < 24) {
+                    this.items = JSON.parse(menuLocalStorage);
+                    return;
+                }
+            }
+
             const querySnapshot = await getDocs(collection(db, 'menu'));
             const items = [];
 
@@ -23,6 +38,8 @@ export const useMenuStore = defineStore('menu', {
             });
 
             this.items = items;
+            localStorage.setItem('menu', JSON.stringify(items));
+            localStorage.setItem('menuLastUpdate', new Date().toISOString());
         }
     }
 })
