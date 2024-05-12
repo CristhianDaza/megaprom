ff3894bed3acd98058f8f7e1e50a43790b336d71
Cristhian Daza <cristian.d.b@hotmail.com>
2024-05-12 16:26:45 -0500

    feat(MpTitle): add component title

---
+++ src/components/UI/MpTitle.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: false
+  }
+})
+</script>
+
+<template>
+  <div
+    class="bg-center bg-auto md:bg-cover bg-no-repeat bg-gray-800 bg-blend-multiply"
+    :class="`bg-[url('${image}')]`"
+  >
+    <h1
+      class="flex justify-center font-extrabold text-3xl md:text-5xl text-white underline underline-offset-3 decoration-blue-600 py-20 my-2">
+      {{ title }}
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
