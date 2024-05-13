8bc6daa4aed00e9beea0af20c34a1fdaed8b79be
Cristhian Daza <cristian.d.b@hotmail.com>
2024-05-13 13:29:35 -0500

    feat(MpTitle.vue): add value default in title

--- src/components/UI/MpTitle.vue
+++ src/components/UI/MpTitle.vue
@@ -13,6 +13,7 @@ const props = defineProps({
 })
 
 const mainTitle = computed(() => {
+  if (!props.title) return 'Productos'
   return props.title.trim().charAt(0).toUpperCase() + props.title.slice(1).toLowerCase()
 })
 </script>
