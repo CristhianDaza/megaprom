d0c62fa8c49dda6a6cdcebf3da951059e05300bf
Cristhian Daza <cristian.d.b@hotmail.com>
2024-05-18 10:25:06 -0500

    fix(Search.vue): fix title to search

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -37,7 +37,7 @@ onMounted(async () => {
 <template>
   <MpBreadcrumb :model="breadcrumbItems" />
   <MpTitle
-    :title="route.query.q ? `Resultados para: ${route.query.q}` : 'Productos'"
+    :title="route.query.q ? route.query.q : 'Productos'"
     image="https://firebasestorage.googleapis.com/v0/b/megaprom-dev.appspot.com/o/web1-06.jpg?alt=media&token=1522ffd6-f6d8-4151-b287-1b905fa58a2c"
   />
   <div v-if="products.isLoading" class="container mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
