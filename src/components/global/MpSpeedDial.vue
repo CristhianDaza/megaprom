9da0fb5ab75efe03ab9eb0b0945737072e31efb2
Cristhian Daza <cristian.d.b@hotmail.com>
2024-05-19 14:47:45 -0500

    feat(MpSpeedDial.vue): add border

--- src/components/global/MpSpeedDial.vue
+++ src/components/global/MpSpeedDial.vue
@@ -20,7 +20,7 @@ const scrollToTop = () => {
   <button
     v-show="showButton"
     @click="scrollToTop"
-    class="fixed bottom-8 right-8 w-12 h-12 dark:bg-gray-50 bg-gray-800 text-white dark:text-gray-800 rounded-md flex items-center justify-center transition-all duration-300 opacity-0 visibility-hidden hover:bg-gray-600"
+    class="fixed bottom-8 right-8 w-12 h-12 dark:bg-gray-50 bg-gray-800 text-white dark:text-gray-800 rounded-md flex items-center justify-center transition-all duration-300 opacity-0 visibility-hidden hover:bg-gray-600 border border-gray-800"
     :class="{ 'opacity-100 visibility-visible': showButton }"
   >
     <span class="text-2xl">
