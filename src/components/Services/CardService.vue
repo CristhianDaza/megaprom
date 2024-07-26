77f188aca9f604df2c2105c238a8ba1d216f2d90
Cristhian Daza <cristian.d.b@hotmail.com>
2024-07-25 23:04:13 -0500

    feat(services): add service promocionales and offset

---
+++ src/components/Services/CardService.vue
@@ -0,0 +1,44 @@
+<script setup>
+const props = defineProps({
+  imgBg: {
+    type: String,
+    required: true
+  },
+  titleService: {
+    type: String,
+    required: true
+  },
+  descriptionService: {
+    type: String,
+  },
+  colorGradient: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="flex group overflow-hidden">
+    <div
+      class="w-[580px] h-[280px] bg-cover bg-center bg-no-repeat bg-gray-400 bg-blend-multiply rounded-3xl text-white p-2 z-20"
+      :style="{ backgroundImage: `url('${imgBg}')` }"
+    >
+      <div class="flex justify-center items-center w-full h-full">
+        <h2 class="text-[3.2rem] font-extrabold text-center group-hover:translate-x-[5000px] transition-all duration-300">{{ titleService.toUpperCase() }}</h2>
+      </div>
+    </div>
+    <div class="w-[20px] max-h-[280px] ml-[-20px] bg-white bg-opacity-80 rounded-br-3xl translate-x-[-20px] group-hover:translate-x-[0] group-hover:w-[680px] transition-all duration-300">
+      <div class="w-full bg-[#0E2050] p-2 text-white group-hover:pl-10 bg-gradient-to-r rounded-tr-3xl" :class="colorGradient">
+        <h2 class="text-[1.8rem] font-extrabold translate-x-[-5000px] group-hover:translate-x-[1px]  transition-all duration-300">{{ titleService.toUpperCase() }}</h2>
+      </div>
+      <div class="pt-8 pl-10 pr-5 max-h-[195px] overflow-y-auto">
+        <p v-html="descriptionService"></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/Service.vue
+++ src/views/Service.vue
@@ -3,10 +3,15 @@ import { useRoute } from 'vue-router'
 import { useHead } from '@unhead/vue'
 import {defineAsyncComponent} from "vue";
 
+import { services } from '@/helpers'
+
 const MpBreadcrumb = defineAsyncComponent(/* webpackChunkName: "mpBreadcrumb" */() => import('@/components/UI/MpBreadcrumb.vue'))
+const CardService = defineAsyncComponent(/* webpackChunkName: "cardService" */() => import('@/components/Services/CardService.vue'))
 const route = useRoute()
 
 const title = `${route.params.serviceId.charAt(0).toUpperCase()}${route.params.serviceId.slice(1)}`
+const titleLower = title.toLowerCase()
+const imageBg = 'https://firebasestorage.googleapis.com/v0/b/mega2024-6a453.appspot.com/o/services%2Fpromocionales%2Fpencil.png?alt=media&token=9b9f40cd-9663-498d-afc2-c9d121c51da2'
 
 const breadcrumbItems = [
   {
@@ -19,6 +24,32 @@ const breadcrumbItems = [
     label: title
   }
 ]
+
+const getColor = (title) => {
+  switch (title.toLowerCase()) {
+    case 'promocionales':
+      return '[#5BC5F2]'
+    case 'offset':
+      return '[#FFCC00]'
+    case 'digital':
+      return '[#E6007D]'
+    default:
+      return 'black'
+  }
+}
+
+const colorGradient = (title) => {
+  switch (title.toLowerCase()) {
+    case 'promocionales':
+      return 'from-[#49C9F4] to-[#134B92]'
+    case 'offset':
+      return 'from-[#7A2D74] to-[#143E88]'
+    case 'digital':
+      return 'from-pink-500 to-pink-600'
+    default:
+      return 'from-gray-500 to-gray-600'
+  }
+}
 
 useHead({
   title: `${title} | Megapromocionales`,
@@ -38,8 +69,29 @@ useHead({
 
 <template>
   <MpBreadcrumb :model="breadcrumbItems" />
-  <div>
-    <h1>Service {{ route.params.serviceId }}</h1>
+  <div
+    :style="{ backgroundImage: title.toLowerCase() === 'promocionales' ? `url('${imageBg}')` : '' }"
+    class="bg-contain w-full h-full bg-right bg-no-repeat bg-blend-multiply"
+  >
+    <h1
+      class="float-right text-[2.7rem] font-extrabold mt-5 mr-12"
+      :class="`text-${getColor(title)}`"
+    >
+      {{ title.toUpperCase() }}
+    </h1>
+    <div class="mt-[8rem]">
+      <div
+        v-for="service in services[titleLower].page1"
+        class="container mx-auto flex flex-row justify-center mb-3"
+      >
+        <CardService
+          :imgBg="service.image"
+          :titleService="service.title"
+          :descriptionService="service.description"
+          :colorGradient="colorGradient(title)"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
